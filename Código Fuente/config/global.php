5fe93f0797483b542c739bb2d9d4fdaa00a4cafb
sonicboy7519 <65363183+sonicboy7519@users.noreply.github.com>
2021-03-05 16:13:28 -0600

    Update global.php

--- Código Fuente/config/global.php
+++ Código Fuente/config/global.php
@@ -3,13 +3,13 @@
 define("DB_HOST","localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "epiz_28086253_dbsistema");
+define("DB_NAME", "dbsistema");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "epiz_28086253");
+define("DB_USERNAME", "root");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "creaciones2021");
+define("DB_PASSWORD", "");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
